4939f91e13268d6ae564f01c45e99402383437a8
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-05 20:13:46 +0500

    handle login process

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-//Register
+// Register
 
 router.post('/register', async (req, res) => {
   try {
@@ -18,6 +18,26 @@ router.post('/register', async (req, res) => {
     const user = await newUser.save();
 
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    !user && res.status(400).json('Wrong credentials');
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+
+    !validated && res.status(400).json('Wrong credentials');
+
+    const { password, ...otherCredentials } = user._doc;
+
+    res.status(200).json(otherCredentials);
   } catch (error) {
     res.status(500).json(error);
   }
