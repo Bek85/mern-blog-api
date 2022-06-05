78eb93ae5dbdfbcde1bd1eade33e1c3f5edec072
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-06 02:53:54 +0500

    use multuer to handle file uploading

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
@@ -18,6 +19,20 @@ mongoose
   })
   .then(console.log('connected to MongoDB'))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, 'test.jpeg');
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
