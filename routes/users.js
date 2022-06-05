26a4c9a4a95872e0b173c323f6fd5bbe5a6fd1a8
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-06 00:12:59 +0500

    delete a particular user with their related posts

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const Post = require('../models/Post');
 
 // Update user
 
@@ -30,9 +31,22 @@ router.put('/:id', async (req, res) => {
 // Delete user
 
 router.delete('/:id', async (req, res) => {
-  try {
-  } catch (error) {
-    res.status(500).json(error);
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.body.userId);
+        res.status(200).json('User has been deleted');
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } catch (error) {
+      res.status(404).json('User not found');
+    }
+  } else {
+    res.status(401).json('You can delete only your account');
   }
 });
 
