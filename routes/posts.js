7a323c7f2f5cbd6fc191e8edbea621f3a40c56a1
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-06 01:54:40 +0500

    handle a route to update a post of the user with given id

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,30 @@ router.post('/', async (req, res) => {
 
 // Update a post
 
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(401).json('You can update only your post');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // Delete a post
 router.delete('/:id', async (req, res) => {
   if (req.body.userId === req.params.id) {
